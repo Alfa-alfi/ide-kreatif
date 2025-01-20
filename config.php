f5cc04a1953edb6a802e8c1ef734bba004ce0d58
alfa-alfi-nurjanah <alfa.alfi124@smk.belajar.id>
2025-01-20 12:04:46 +0700

    menambahkan config

--- config.php
+++ config.php
@@ -4,10 +4,10 @@
 $host = "localhost"; // nama host server database
 $username = "root"; // username untuk akses database
 $password = ""; // password untuk akses database
-$database = "ideKreatif"; // nama database yang digunakan
+$database = "idekreatif"; // nama database yang digunakan
 
 // membuat koneksi ke database menggunakan MySQL i
-$con = mysqli_connect($host, $username, $password, $database);
+$conn = mysqli_connect($host, $username, $password, $database);
 
 // mengecek apakah koneksi berhasil
 if ($conn->connect_error) {
