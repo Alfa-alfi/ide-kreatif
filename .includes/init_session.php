92ce8fb500415ecaa202e14c48f26a28a8845656
alfa-alfi-nurjanah <alfa.alfi124@smk.belajar.id>
2025-02-03 12:26:34 +0700

    menambahkan init

--- .includes/init_session.php
+++ .includes/init_session.php
@@ -8,3 +8,8 @@ $notification = $_SESSION['notification'] ?? null;
 if ($notification) {
     unset($_SESSION['notification']);
 }
+if (empty($_SESSION["username"]) || empety($_SESSION["role"])) {
+    $_SESSION['notification'] = ['type' => 'danger', 'message' => 'Silahkan Login Terlebih Dahulu!'];
+    header('Location: ./auth/login.php');
+    exit();
+}
