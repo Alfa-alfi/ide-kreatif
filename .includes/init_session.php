66d96d3e3cc896f6d89e68da68ccc660cc18e3d5
alfa-alfi-nurjanah <alfa.alfi124@smk.belajar.id>
2025-02-03 11:10:30 +0700

    menambahkan init

--- .includes/init_session.php
+++ .includes/init_session.php
@@ -4,7 +4,7 @@ session_start();
 $name = $_SESSION["name"];
 $role = $_SESSION["role"];
 // ambil notifikasi jika ada,kemudian hapus dari sesi
-$notification =$_SESSION['notification'] ?? null;
+$notification = $_SESSION['notification'] ?? null;
 if ($notification) {
     unset($_SESSION['notification']);
 }
