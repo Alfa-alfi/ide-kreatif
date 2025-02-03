7d1f590cb871d80a6ea16125e4504e2357ef47ed
alfa-alfi-nurjanah <alfa.alfi124@smk.belajar.id>
2025-02-03 11:01:50 +0700

    menambahkan fungsi init

--- .includes/init_session.php
+++ .includes/init_session.php
@@ -4,7 +4,7 @@ session_start();
 $name = $_session["name"];
 $role = $_session["role"];
 // ambil notifikasi jika ada,kemudian hapus dari sesi
-$notification =$_session['notification'] ?? null;
+$notification =$_SESSION['notification'] ?? null;
 if ($notification) {
-    unset($_session['notification']);
+    unset($_SESSION['notification']);
 }
