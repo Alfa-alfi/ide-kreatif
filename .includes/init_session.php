4bb73ab7e7786ad9d4745e52282fd2ab3da286d5
alfa-alfi-nurjanah <alfa.alfi124@smk.belajar.id>
2025-02-09 21:07:47 +0700

    menambahkan init

--- .includes/init_session.php
+++ .includes/init_session.php
@@ -1,14 +1,16 @@
 <?php
 session_start();
-
+// ambil data dari sesi
 $userId = $_SESSION["user_id"];
 $name = $_SESSION["name"];
 $role = $_SESSION["role"];
 //ambil notifikasi jika ada, kemudian hapus dari sesi
-$notification = $_SESSION['notification']?? null;
+$notification = $_SESSION['notification'] ?? null;
 if ($notification){
     unset ($_SESSION['notification']);
 }
+/* periksa apakah sesi username dan role sudah ada, 
+jika tidak arahkan ke halaman login */
 if (empty($_SESSION["username"]) || empty($_SESSION["role"])) {
     $_SESSION['notificcation'] = [
         'type' => 'danger',
