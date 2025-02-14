044dcbcf2408de906655d5087491c21a950c5641
alfa-alfi-nurjanah <alfa.alfi124@smk.belajar.id>
2025-02-14 10:16:01 +0700

    menambahkan register

--- auth/register_process.php
+++ auth/register_process.php
@@ -1,6 +1,6 @@
 <?php
 require_once("../config.php");
-
+// memulai session
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -8,17 +8,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $password = $_POST["password"];
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    $sql = "INSERT INTO users (username, name, password)
+    VALUES ('$username', '$name', '$hashedPassword')";
+   if ($conn->query($sql) === TRUE){
+    // simpan notifikasi ke dalam session
+   }
 
-    $sql = "INSERT INTO users (username, name, password) VALUES('$username', '$name', '$hashedPassword')";
-    if ($conn->query($sql) === TRUE) {
-        //simpan notifikasi ke dalam session
-        $_SESSION['notification'] = ['type' => 'primary', 'message' => 'Registrasi Berhasil!' ];
+        $_SESSION['notification'] = [
+            'type' => 'primary',
+            'message' => 'Registrasi Berhasil!'
+        ];
     } else {
-        $_SESSION['notification'] = ['type' => 'danger', 'message' => 'Gagal Registrasi: ' . mysqli_error($conn)];
+        $_SESSION['notification'] = [
+            'type' => 'danger',
+            'message' => 'Gagal Registrasi: ' . mysqli_error($conn)
+        ];
     }
     header('Location: login.php');
     exit();
-}
 
 $conn->close();
 ?>
